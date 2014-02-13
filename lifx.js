e2fa5055ee52128045df0f8775ee6dc2b538d4d1
Jean-Philippe Encausse <Jp@encausse.net>
2014-02-14 00:12:05 +0100

    First Introduce plugin LIFX

---
+++ lifx.js
@@ -0,0 +1,208 @@
+// ------------------------------------------
+//  SETUP
+// ------------------------------------------
+
+var lx = false;
+exports.init = function(SARAH){
+  
+  if (lx) return;
+  
+  var lifx = require('./lib');
+  lifx.setDebug(false);
+  
+  lx = lifx.init();
+  lx.on('bulbstate', function(b) {
+    //console.log('Bulb state: ' + util.inspect(b));
+  });
+  
+  lx.on('bulbonoff', function(b) {
+    //console.log('Bulb on/off: ' + util.inspect(b));
+  });
+  
+  lx.on('bulb', function(b) {
+    console.log('New bulb found: ' + b.name);
+  });
+  
+  lx.on('gateway', function(g) {
+    console.log('New gateway found: ' + g.ipAddress.ip);
+  });
+
+  lx.on('packet', function(p) {
+    // Show informational packets
+    switch (p.packetTypeShortName) {
+      case 'powerState':
+      case 'wifiInfo':
+      case 'wifiFirmwareState':
+      case 'wifiState':
+      case 'accessPoint':
+      case 'bulbLabel':
+      case 'tags':
+      case 'tagLabels':
+      //case 'lightStatus':
+      case 'timeState':
+      case 'resetSwitchState':
+      case 'meshInfo':
+      case 'meshFirmware':
+      case 'versionState':
+      case 'infoState':
+      case 'mcuRailVoltage':
+        console.log(p.packetTypeName + " - " + p.preamble.bulbAddress.toString('hex') + " - " + util.inspect(p.payload));
+        break;
+    }
+  });
+}
+
+// ------------------------------------------
+//  COLOR ALGORITHM
+// ------------------------------------------
+
+function hexStringToRgb(s) {
+    return {
+        r: parseInt(s.substring(0, 2), 16) / 255,
+        g: parseInt(s.substring(2, 4), 16) / 255,
+        b: parseInt(s.substring(4, 6), 16) / 255
+    };
+}
+
+var rgbToHsl = function(r, g, b){
+  r /= 255, g /= 255, b /= 255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if(max == min){
+      h = s = 0; // achromatic
+  } else {
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch(max){
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+  }
+  return [h, s, l];
+}
+
+// ------------------------------------------
+//  INTERFACE
+// ------------------------------------------
+
+var util = require('util');
+var packet = require('./lib/packet');
+var cycledColour = 0;
+exports.action = function(data, callback, config, SARAH){
+  
+  var config = config.modules.lifx;
+  if (false){
+    return callback('tts', 'Configuration LIFX invalide');
+  }
+console.log(data);
+  // Switch On / Off
+  if (data.on == "true"){
+    console.log("Lights on");
+    lx.lightsOn(); 
+  } else { 
+    console.log("Lights off");
+    lx.lightsOff();  
+  }
+  
+  // Dim Color: hue, sat, lum, whitecol, timing, (bulb)
+  if (data.rgb){
+    console.log("Dim "+data.rgb);
+    
+    var rgb = hexStringToRgb(data.rgb);
+    var hsl = rgbToHsl(rgb.r, rgb.g, rgb.b);
+    console.log(rgb,' => ',hsl);
+    
+    data.hue = Math.round(hsl[0] * 65535);
+    data.sat = Math.round(hsl[1] * 65535);
+    data.bri = Math.round(hsl[2] * 65535);
+    data.bri = Math.round(data.bri / 260 * 65535);
+    
+    var hue = '0x'+data.hue.toString(16);
+    var sat = '0x'+data.sat.toString(16);
+    var lum = '0x'+data.bri.toString(16);
+    
+    lx.lightsColour(hue, sat, lum, 0, 0);
+  }
+  
+  return callback({}); 
+  
+  /*
+  if (data.rgb == "red"){
+    console.log("Dim red");
+    lx.lightsColour(0x0000, 0xffff, 1000, 0, 0);
+  }
+  else if (data.rgb == "purple"){
+    console.log("Dim purple");
+  lx.lightsColour(0xcc15, 0xffff, 500, 0, 0);
+  }
+  else if (data.rgb == "white"){
+    console.log("Dim purple");
+    lx.lightsColour(0x0000, 0x0000, 0x8000, 0x0af0, 0x0513);
+  }
+  else if (data.rgb == "cycle"){
+    cycledColour = (cycledColour+100) & 0xffff; console.log("Colour value is " + cycledColour);
+    lx.lightsColour(cycledColour, 0xffff, 0x0200, 0x0000, 0x0000);
+  }
+  
+  cycledColour = (cycledColour-100) & 0xffff; console.log("Colour value is " + cycledColour);
+  lx.lightsColour(cycledColour, 0xffff, 0x0200, 0x0000, 0x0000);
+  
+  console.log("Enabling debug");
+  lifx.setDebug(true);
+  
+  console.log("Requesting voltage");
+  var message = packet.getMcuRailVoltage();
+  lx.sendToAll(message);
+  
+  console.log("Requesting power state");
+  var message = packet.getPowerState();
+  lx.sendToAll(message);
+  
+  console.log("Requesting wifi info");
+  var message = packet.getWifiInfo();
+  lx.sendToAll(message);
+  
+  console.log("Requesting wifi firmware state");
+  var message = packet.getWifiFirmwareState();
+  lx.sendToAll(message);
+  
+  console.log("Requesting wifi state");
+  var message = packet.getWifiState({'interface':2});
+  lx.sendToAll(message);
+  
+  console.log("Requesting bulb label");
+  var message = packet.getBulbLabel();
+  lx.sendToAll(message);
+  
+  console.log("Requesting tags");
+  var message = packet.getTags();
+  lx.sendToAll(message);
+  
+  console.log("Requesting tag label for tag 1");
+  var message = packet.getTagLabels({tags:new Buffer([1,0,0,0,0,0,0,0])});
+  lx.sendToAll(message);
+  
+  console.log("Requesting time");
+  var message = packet.getTime();
+  lx.sendToAll(message);
+  
+  console.log("Requesting info");
+  var message = packet.getInfo();
+  lx.sendToAll(message);
+  
+  console.log("Requesting reset switch state");
+  var message = packet.getResetSwitchState();
+  lx.sendToAll(message);
+  
+  console.log("Requesting mesh info");
+  var message = packet.getMeshInfo();
+  lx.sendToAll(message);
+  
+  console.log("Requesting mesh firmware");
+  var message = packet.getMeshFirmware();
+  lx.sendToAll(message);
+  */
+}
